22ac1a66e941f160576009bf961da4b854c7e869
grjorge <deb.devel@outlook.es>
2024-06-08 00:04:34 -0600

    Configurations and server

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { json, urlencoded } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(urlencoded({ extended: false }));
+app.use(morgan('dev'));
+
+export default app;
